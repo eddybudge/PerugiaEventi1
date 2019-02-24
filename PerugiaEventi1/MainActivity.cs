af22473981a2e12dd5979f4b78009c5e76fae587
eddybudge <kozak.yana96@gmail.com>
2019-02-24 17:12:55 +0100

    ..

--- PerugiaEventi1/MainActivity.cs
+++ PerugiaEventi1/MainActivity.cs
@@ -4,6 +4,10 @@ using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
 using Android.Net;
+using System.IO;
+using PerugiaEventi1.Model;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace PerugiaEventi1
 {
@@ -12,6 +16,11 @@ namespace PerugiaEventi1
     {
         Button bottoneCaricaEventi;
         ListView listaEventi;
+        string response;
+        RootObject root;
+        List<Contenuto> listaContenuti;
+        Contenuto primoContenuto;
+        string totaleContenuti;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -21,7 +30,21 @@ namespace PerugiaEventi1
 
             bottoneCaricaEventi = FindViewById<Button>(Resource.Id.bottoneCaricaEventi);
             listaEventi = FindViewById<ListView>(Resource.Id.listaEventi);
+            bottoneCaricaEventi.Click += delegate {
+                //bottoneCaricaEventi.Text = "Sto caricando";
+               
+                //carica json locale e deserializzalo
 
+                StreamReader strm = new StreamReader(Assets.
+                    Open("eventijsonitit.zipeventiitit.json"));
+                response = strm.ReadToEnd();
+
+                root = JsonConvert.DeserializeObject<RootObject>(response);
+                totaleContenuti = root.Total;
+                listaContenuti = root.Contenuto;
+                primoContenuto = listaContenuti[0];
+                bottoneCaricaEventi.Text = primoContenuto.Titolo;
+            };
         }
 
     }
