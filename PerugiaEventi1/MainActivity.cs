303c67d50943ed3f4b9010cdff5b9a419f2c6320
eddybudge <kozak.yana96@gmail.com>
2019-02-24 08:04:42 +0100

    ...

--- PerugiaEventi1/MainActivity.cs
+++ PerugiaEventi1/MainActivity.cs
@@ -13,8 +13,6 @@ namespace PerugiaEventi1
         Button bottoneCaricaEventi;
         ListView listaEventi;
 
-        Uri uriEventi = http://dati.umbria.it/dataset/410faa97-546b-4362-a6d7-f8794d18ed19/resource/8afe729a-0f59-4647-95ee-481577e83bea/download/eventijsonitit.zipeventiitit.json;
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -23,11 +21,6 @@ namespace PerugiaEventi1
 
             bottoneCaricaEventi = FindViewById<Button>(Resource.Id.bottoneCaricaEventi);
             listaEventi = FindViewById<ListView>(Resource.Id.listaEventi);
-
-            manager = DownloadManager.FromContext(this);
-
-             request = new DownloadManager.Request(Uri.Parse(uri));
-            long downloadId = manager.Enqueue(request);
         }
     }
 }
